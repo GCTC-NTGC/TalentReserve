95cfacbfd88410964a5ab176afc20010a677b62a
Yonathan Kidanemariam <yoni.kid15@gmail.com>
2020-05-28 12:52:02 -0400

    use lodashes _isNaN helper to fix ie11 error with Number.isNan()

--- resources/assets/js/models/lookupConstants.ts
+++ resources/assets/js/models/lookupConstants.ts
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 export enum ReviewStatusId {
   ScreenedOut = 1,
   StillThinking = 2,
@@ -188,7 +190,7 @@ export function getKeyByValue(object, value): string {
 export function enumToIds(enumType: object): number[] {
   const enumVals = Object.values(enumType);
   // Note: this first array includes the list of ids as strings, followed by the list of names as strings
-  const enumIds = enumVals.filter((item) => !Number.isNaN(Number(item)));
+  const enumIds = enumVals.filter((item) => _.isNaN(Number(item)));
   return enumIds.map((id) => Number(id));
 }
 
